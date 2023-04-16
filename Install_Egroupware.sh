a286dd2ce08f46aeb030e77efa824d2187a8b101
tintin974 <tintin_97410@hotmail.fr>
2023-04-16 17:34:44 +0400

    1.0

--- Install_Egroupware.sh
+++ Install_Egroupware.sh
@@ -3,8 +3,8 @@ mkdir -p script >/dev/null
 mkdir -p script/logs>/dev/null
 mkdir -p script/tips>/dev/null
 
-chmod +x /Deployment_Egroupware/1_Install_Egroupware.sh
-chmod +x /Deployment_Egroupware/2_Install_App_Wiki.sh
+chmod +x Deployment_Egroupware/1_Install_Egroupware.sh
+chmod +x Deployment_Egroupware/2_Install_App_Wiki.sh
 
 
 ./Deployment_Egroupware/1_Install_Egroupware.sh
