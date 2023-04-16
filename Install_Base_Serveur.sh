cc2af43e21c5509b06ae797cb9ee3923a8128bdf
tintin974 <tintin_97410@hotmail.fr>
2023-04-16 17:12:28 +0400

    1.0

--- Install_Base_Serveur.sh
+++ Install_Base_Serveur.sh
@@ -2,10 +2,10 @@
 mkdir -p script >/dev/null
 mkdir -p script/logs>/dev/null
 mkdir -p script/tips>/dev/null
-chmod +x /Base_Serveur/1_Install_Utils.sh
-chmod +x /Base_Serveur/2_Install_LAMP.sh
-chmod +x /Base_Serveur/3_Install_Webmin.sh
-chmod +x /Base_Serveur/4_Install_LDAP.sh
+chmod +x Base_Serveur/1_Install_Utils.sh
+chmod +x Base_Serveur/2_Install_LAMP.sh
+chmod +x Base_Serveur/3_Install_Webmin.sh
+chmod +x Base_Serveur/4_Install_LDAP.sh
 
 ./Base_Serveur/1_Install_Utils.sh
 ./Base_Serveur/2_Install_LAMP.sh
